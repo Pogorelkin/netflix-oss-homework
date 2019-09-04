afe955a175088743e00d9573551c37c3892ed3d9
Vadim <vadim_pogorelkin@epam.com>
2019-09-04 16:47:43 +0400

    init

--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/EmployeesApplication.groovy
@@ -2,8 +2,12 @@ package com.epam.hw.netflix
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 class EmployeesApplication {
     static void main(String[] args) {
         SpringApplication.run(EmployeesApplication, args)

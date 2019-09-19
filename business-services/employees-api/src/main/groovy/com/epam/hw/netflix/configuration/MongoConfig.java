5e528a0a433a15fead0ef35e0753fb72e8911d52
Vadim <vadim_pogorelkin@epam.com>
2019-09-19 13:56:55 +0400

    elk done

---
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/configuration/MongoConfig.java
@@ -0,0 +1,4 @@
+package com.epam.hw.netflix.configuration;
+
+public class MongoConfig {
+}
--- business-services/employees-api/src/main/groovy/com/epam/hw/netflix/controllers/EmployeeAPIController.groovy
+++ business-services/employees-api/src/main/groovy/com/epam/hw/netflix/controllers/EmployeeAPIController.groovy
@@ -2,6 +2,8 @@ package com.epam.hw.netflix.controllers
 
 import com.epam.hw.netflix.api.WorkspaceAPI
 import com.epam.hw.netflix.services.EmployeeService
+import org.apache.logging.log4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/employees")
 class EmployeeAPIController {
+    org.slf4j.Logger logger = LoggerFactory.getLogger(EmployeeAPIController.class)
 
     @Autowired
     EmployeeService employeeService
@@ -20,6 +23,7 @@ class EmployeeAPIController {
     @RequestMapping("/{id}")
     def describeEmployee(@PathVariable("id") String id) {
         def employee = employeeService.findEmployee(id)
+        logger.info("employee service found employee " + employee.toString())
         [
                 id       : employee.id,
                 firstName: employee.firstName,
